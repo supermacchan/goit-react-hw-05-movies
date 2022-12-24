8e56793456489f70236a7d6e217a4b3fe161bf7a
Olga Birkovska <birkovskaya92@ukr.net>
2022-12-24 21:45:35 +0200

    trending list styling + header icon

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,15 @@
 import { NavLink } from "react-router-dom";
 import css from "./Header.module.css";
+import { MdMovieFilter } from 'react-icons/md';
 
 export const Header = () => {
     return (
         <header className={css.header}>
             <nav className={css.header__nav}>
-                <NavLink to="/" className={css.header__link}>Home</NavLink>
+                <NavLink to="/" className={css.header__link}>
+                    <MdMovieFilter className={css.header__icon} />
+                    Home
+                </NavLink>
                 <NavLink to="/movies" className={css.header__link}>Movies</NavLink>
             </nav>
         </header>
