60e98c32b7457c0e0263de15cd89e88e8e1408be
Olga Birkovska <birkovskaya92@ukr.net>
2022-12-25 19:29:15 +0200

    search form

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,8 +1,53 @@
-// import css from './Movies.module.css';
-import { Outlet } from 'react-router-dom';
+import css from './Movies.module.css';
+import { Outlet, useLocation } from 'react-router-dom';
+import { useState } from 'react';
+import { BsSearch } from 'react-icons/bs';
+import { toast } from 'react-toastify';
+
 
 export const Movies = () => {
+    const location = useLocation();
+    // console.log(location);
+    const [value, setValue] = useState('');
+
+    const handleInputChange = event => {
+        const inputValue = event.currentTarget.value.toLowerCase();
+        setValue(inputValue);
+    };
+
+    const handleFormSubmit = event => {
+        event.preventDefault();
+        if (value.trim() === '') {
+            toast.error('Enter search query.');
+            reset();
+            return;
+        }
+        // onSubmit(value);
+        reset();
+    };
+
+    const reset = () => {
+        setValue('');
+    };
+
     return (
-        <Outlet />
+        <>
+            {location.pathname === '/movies' &&
+                <form className={css.searchForm} onSubmit={handleFormSubmit}>
+                    <input
+                        type="text"
+                        name="query"
+                        placeholder="Enter movie name"
+                        className={css.input}
+                        onChange={handleInputChange}
+                    />
+                <button type="submit" className={css.button}>
+                    <BsSearch className={css.buttonIcon} />
+                    Search
+                </button>
+            </form>
+            }
+            <Outlet />
+        </>
     );
 }
