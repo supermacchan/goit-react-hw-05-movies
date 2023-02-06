1b03bd4676b7fec3955379a7182035dd6b10c29d
Olga Birkovska <aiba.aki9@gmail.com>
2023-02-06 21:44:18 +0200

    color change

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -8,7 +8,7 @@ export const Loader = () => {
                 height="80" 
                 width="80" 
                 radius="9"
-                color="#015201" 
+                color="#B92009" 
                 ariaLabel="three-dots-loading"
                 wrapperStyle={{}}
                 wrapperClassName=""
